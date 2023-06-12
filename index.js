18bd051954de691266ec15fb38a7ebb1bb498fac
Alec Barker <avbbarker@icloud.com>
2023-06-12 19:21:40 -0400

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400){
+    return "This one is on me!"
+  }
+  else if (400 < distance && distance < 2000){
+    return "That will be twenty bucks."
+  } 
+  else if (2000 < distance && distance < 2500){
+    return "I will gladly take your thirty bucks."
+  } 
+  else (distance >= 2500);{
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(isCity){
+  const city = 'NYC';
+  return (isCity === city ? "Ok, sounds good." : "No go.")
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
